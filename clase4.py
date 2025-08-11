03e0943aed37102077d05662ce840f76ccf2cd03
Daniel <danialexander.dev@gmail.com>
2025-08-10 22:55:23 -0500

    Avances de las clases python

---
+++ clase4.py
@@ -0,0 +1,9 @@
+name='DANIEL ALEX'
+last_name='Renteria Pino'
+print(5*name)
+print(name +''+last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower()) # funcion que convierte la cadena de caracteres a minuscula
+
+
