6c9c46d118faa92a1001b5b21258c10885fe0623
Daniel <danialexander.dev@gmail.com>
2025-08-10 22:28:58 -0500

    Cree los archivos de las clases practicas y ejecicios interactivos

---
+++ clase3.py
@@ -0,0 +1,8 @@
+#Voy a crear variables para el saludo
+saludo='Hola que tal'
+nombre='Daniel'
+# Voy a imprimir saludo+nombre
+print(saludo + nombre)
+# Forma tradicional
+print("Hola Mundo")
+print("Daniel")
